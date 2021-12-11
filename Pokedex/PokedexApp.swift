a495faddc16e61113e3bfd2513ce7e246247fbc0
Klaudia Synarska <klaudiasynarska@wp.pl>
2021-12-11 18:53:34 +0100

    Initial commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Klaudia Synarska on 11/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
